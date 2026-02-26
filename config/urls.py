0ff3b1492705d6692ff3664bf96256b737d34ec1
JohnnyUtah5551 <johnny79126044093@gmail.com>
2026-02-26 16:29:41 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,1 +1,12 @@
+"""
+Главный файл с маршрутами всего проекта
+"""
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),        # Все маршруты для пользователей
+    path('api/auth/', include('auth_system.urls')),   # Маршруты для управления правами
+    path('api/shop/', include('shop.urls')),          # Демо-магазин
+]
