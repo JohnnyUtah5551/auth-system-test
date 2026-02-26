745b0bd0ccc3b50050e19eb892a4f2733273841e
JohnnyUtah5551 <johnny79126044093@gmail.com>
2026-02-26 16:31:00 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,1 +1,13 @@
+"""
+Маршруты для демо-магазина
+"""
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('products/create/', views.ProductCreateView.as_view(), name='product-create'),
+    path('products/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product-update'),
+    path('products/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product-delete'),
+]
